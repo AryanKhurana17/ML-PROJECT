f2cfbab7610a6ecd7b5dbdb0e8c797cc733f9ec2
AryanKhurana17 <aryankhurana1701@gmail.com>
2025-03-18 01:41:16 +0530

    Model training and testing

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,7 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 @dataclass    #used to directly define our class variable
 class DataIngestionConfig:    #input given , later : will save our train file here
@@ -52,7 +53,10 @@ if __name__ == "__main__":
     train_data,test_data = obj.initiate_data_ingestion() 
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr, test_arr, preprocessor= data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
 #python -m src.components.data_ingestion --> use -m to run this file . Why? It will search for modules inside this directory and not in sys
     
